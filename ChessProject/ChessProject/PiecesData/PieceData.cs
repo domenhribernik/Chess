be3fa663a3ae2956e285d01b7610ccf39a4039b1
KlemenFelkner <klemen.felkner@student.um.si>
2022-12-12 19:17:22 +0100

    Generalizations on functions, other pieces added, but need to be edited

---
+++ ChessProject/ChessProject/PiecesData/PieceData.cs
@@ -0,0 +1,21 @@
+﻿namespace ChessProject.PiecesData
+{
+    public class PieceData
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Direction Direction { get; set; }
+        public string Color { get; set; }
+        public Type Type { get; set; }
+    }
+
+    public enum Direction
+    {
+        Up, Diagonal, Down, Diagonally, OneAny, LShape, Any, Straight
+    }
+
+    public enum Type
+    {
+        Pawn, Bishop, King, Knight, Rook, Queen
+    }
+}
